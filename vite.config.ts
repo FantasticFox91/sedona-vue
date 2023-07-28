e3b8768d18b059e84c9e63bfd3c021c7f5750a36
FantasticFox91 <44309782+FantasticFox91@users.noreply.github.com>
2023-07-28 09:18:46 +0300

    working on copy vido to video player

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/video.mp4',
+          dest: 'assets'
+        }
+      ]
+    })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
